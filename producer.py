d47f2304e7e9472fad2ec4709b47dbfa06aa51a2
Mathias Sass Michno <mmichn13@student.aau.dk>
2016-12-05 13:03:16 +0100

    Update producer mock to new server and add error handling

--- producer.py
+++ producer.py
@@ -20,8 +20,11 @@ def make_new_user(server, superuser):
             "surname"  : "McTestface",
             "password" : "hunter2"
     }
-
     r = requests.post(server + '/user', json=user_data, headers=su_auth_header)
+    if r.status_code is not 200:
+        print('\n--{:=^50}--'.format(' New User NOT Created '))
+        print(r.json()['message'])
+        sys.exit(1)
     print('\n--{:=^50}--'.format(' New User Created '))
     pprint(r.json())
     return r.json()['username'], "hunter2"
@@ -29,6 +32,10 @@ def make_new_user(server, superuser):
 def authenticate_user(server, user):
     user_data = {"username": user[0], "password": user[1]}
     r = requests.post(server + '/auth', json=user_data)
+    if r.status_code is not 200:
+        print('\n--{:=^50}--'.format(' User NOT Authenticated '))
+        print(r.json()['message'])
+        sys.exit(1)
     print('\n--{:=^50}--'.format(' User Authenticated '))
     pprint(r.json())
     return {"Authorization": "Sleepy token=" + r.json()['token']}
@@ -41,16 +48,34 @@ def make_vehicle(server, auth_header):
             "vin" : fake.ean13()
     }
     r = requests.post(server + '/vehicle', json=car_data, headers=auth_header)
+    if r.status_code is not 200:
+        print('\n--{:=^50}--'.format(' Vehicle NOT Created '))
+        print(r.json()['message'])
+        sys.exit(1)
     print('\n--{:=^50}--'.format(' Vehicle Created '))
     pprint(r.json())
-    return r.json()['id']
+    return str(r.json()['id'])
 
 def make_route(server, auth_header, vehicle_id):
-    route_data = { "vehicleid": vehicle_id }
+    route_data = { "vehicleid": vehicle_id, "routeState": "CREATED" }
     r = requests.post(server + '/route', json=route_data, headers=auth_header)
+    if r.status_code is not 200:
+        print('\n--{:=^50}--'.format(' Route NOT Created '))
+        print(r.json()['message'])
+        sys.exit(1)
     print('\n--{:=^50}--'.format(' Route Created '))
     pprint(r.json())
-    return r.json()['id']
+    return str(r.json()['id'])
+
+def change_route_state(server, auth_header, route_id, state):
+    route_data = { "routeState": state }
+    r = requests.put(server + '/route/' + route_id, json=route_data, headers=auth_header)
+    if r.status_code is not 200:
+        print('\n--{:=^50}--'.format(' Route State NOT Changed '))
+        print(r.json()['message'])
+        sys.exit(1)
+    print('\n--{:=^50}--'.format(' Route State Changed '))
+    pprint(r.json())
 
 def make_waypoint(server, auth_header, route_id, latitude, longitude):
     waypoint_data = {
@@ -59,8 +84,12 @@ def make_waypoint(server, auth_header, route_id, latitude, longitude):
             "timestamp": int(time.time() * 1000)
     }
     r = requests.post(server + '/route/' + route_id + '/waypoint', json=waypoint_data, headers=auth_header)
-    print('\n--{:=^50}--'.format(' Waypoint Created '))
-    pprint(r.json())
+    if r.status_code is not 200:
+        print('\n--{:=^50}--'.format(' Waypoint NOT Created '))
+        print(r.json()['message'])
+    else:
+        print('\n--{:=^50}--'.format(' Waypoint Created '))
+        pprint(r.json())
 
 def get_route_from_google_maps(start, end, force=False):
     import googlemaps
@@ -70,7 +99,11 @@ def get_route_from_google_maps(start, end, force=False):
         sys.exit(0)
     api_key = os.environ['GOOGLE_MAPS_API_KEY']
     gmaps = googlemaps.Client(key=api_key)
-    directions = gmaps.directions(start, end, mode="driving", alternatives=True)
+    try:
+        directions = gmaps.directions(start, end, mode="driving", alternatives=True)
+    except Exception as e:
+        print("ERROR: ", str(e))
+        sys.exit(1)
     route = None
     if not force and len(directions) > 1:
         print("Multiple routes found.\nChoose one:")
@@ -104,7 +137,7 @@ def get_route_from_gpx_file(file):
     return coordinates, len(coordinates)
 
 @begin.start(auto_convert=True)
-def main(server: 'URL of the server' = "http://172.25.11.114:8080",
+def main(server: 'URL of the server' = "http://sw708e16.cs.aau.dk",
          user: 'If not supplied a new user will be made' = (None, None),
          superuser: 'Used to make new user' = ("deadpool", "hunter2"),
          waypoints: 'GPX file to read waypoints from' = None,
@@ -120,7 +153,7 @@ def main(server: 'URL of the server' = "http://172.25.11.114:8080",
         points, duration = get_route_from_gpx_file(waypoints)
     else:
         print("You must use either start and end point or gpx file with waypoints.")
-        return
+        sys.exit(1)
     delay = delay if delay > 0 else duration / len(points)
     server = urljoin(server, base_path)
     print(server)
@@ -129,8 +162,13 @@ def main(server: 'URL of the server' = "http://172.25.11.114:8080",
     auth_header = authenticate_user(server, user)
     vehicle_id = make_vehicle(server, auth_header)
     route_id = make_route(server, auth_header, vehicle_id)
+    change_route_state(server, auth_header, route_id, "ACTIVE")
 
     for i, point in enumerate(points):
+        t = time.time()
         make_waypoint(server, auth_header, route_id, latitude=point[0], longitude=point[1])
         print('{} out of {} | estimated time left: {}'.format(i+1, len(points), timedelta(seconds= delay * (len(points) - i))))
-        time.sleep(delay)
+        if time.time() - t < delay:
+            time.sleep(delay - (time.time() - t))
+
+    change_route_state(server, auth_header, route_id, "COMPLETE")
